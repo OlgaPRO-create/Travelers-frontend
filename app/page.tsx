f1af96f0f906a79831bec9e1d61efbf57afbea40
OlgaPRO-create <obrizhak2016@gmail.com>
2026-02-24 00:56:38 +0200

    test and fixed file

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import Block from '../components/Block/Block';
+import StoriesList from '../components/StoriesList/StoriesList';
+import TravelersList from '../components/TravellersList/TravellersList';
+import Join from '../components/Join/Join';
+import Hero from '@/components/Hero/Hero';
+import About from '@/components/About/About';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+import { getUsers } from '@/lib/api/api';
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['users'],
+    queryFn: () => getUsers({}),
+  });
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <Hero />
+        <About />
+        <Block title="Популярні історії">
+          <StoriesList />
+        </Block>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <Block title="Наші Мандрівники">
+            <TravelersList />
+          </Block>
+        </HydrationBoundary>
+        <Join />
       </main>
     </div>
   );
---
+++ types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  avatarUrl: string;
+  description: string;
+  articlesAmount: number;
+  savedArticles: string[];
+  _id: string;
+}
